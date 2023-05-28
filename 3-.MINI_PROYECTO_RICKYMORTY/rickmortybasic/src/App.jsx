90aa660d2d4262c05f9a5c347e8049874b56195c
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-05-29 00:33:34 +0200

    useEffect added

--- 3-.MINI_PROYECTO_RICKYMORTY/rickmortybasic/src/App.jsx
+++ 3-.MINI_PROYECTO_RICKYMORTY/rickmortybasic/src/App.jsx
@@ -20,7 +20,15 @@ const App = () => {
 const [characterList, setCharacterList] = React.useState(charactersMock);
   const characters = myState[0];
   const charactersSet = myState[1];
+React.useEffect(()=> {  //El useEffect lo que nos va permitir es tener disponible el ciclo de vida de nuestro componente para ejectuar funciones en distintos momentos.Este hook se lanzara siempre y cuando se haya creado el componente, es decir, cuando está montado, de tal modo que nos permite pasar info al componente y React se encargará de re-renderizar con los datos obtenidos sin bloqeuar la vista inicial al cliente web.
+  (async () => {
+    let data = await fetch('https://rickandmortyapi.com/api/character/').then(
+      (res) => res.json()
+    );
+    setCharacterList(data.results);
 
+ })();
+  }, []);
   //Haremos un map de characters para pintarlos en el dom.
   //añadimos 'key={character.id}' para generar una key unica en cada elemento. Las key son una información adicional que podemos añadir a nuestros elementos de React para gestionar adecuadamente su ordenación en listas dinámicas sin crear bugs inesperados. 
   return (
