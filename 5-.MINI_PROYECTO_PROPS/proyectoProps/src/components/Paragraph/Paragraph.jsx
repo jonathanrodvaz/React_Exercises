cfb2f4d8333a95376007f6370b03727624fbe59b
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-05-30 19:32:38 +0200

    Props practice

--- 5-.MINI_PROYECTO_PROPS/proyectoProps/src/components/Paragraph/Paragraph.jsx
+++ 5-.MINI_PROYECTO_PROPS/proyectoProps/src/components/Paragraph/Paragraph.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Paragraph = () => {
+    let cheerMessage = 'Lets go!'
+    let paragraphMessage = 'Hi! This is a project to practice with the React concept of props!'
     return (
         <div>
-            <p>Hi! This is a project to practice with the React concept of props! Let's go!</p>
+            <p>{paragraphMessage} {cheerMessage}</p>
         </div>
     )
 }
