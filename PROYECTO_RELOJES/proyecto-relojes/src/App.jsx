dbd3fb917ad46c89ee818aa519fc7999305a12ca
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-06-03 14:34:45 +0200

    DigitalClock WIP

---
+++ PROYECTO_RELOJES/proyecto-relojes/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import DigitalClock from './components/DigitalClock/DigitalClock.jsx'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <DigitalClock digitalClock={DigitalClock}/>
+    </>
+  )
+}
+
+export default App
---
+++ PROYECTO_RELOJES/proyecto-relojes/src/components/DigitalClock/DigitalClock.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'; //Debemos traernos useState para que seteé la hora a traves de una función, y por otro lado debemos traernos useEffect para que recoja la hora local a traves de una función y la ejecute al cargar la pagina y al pasar a traves del intervalo de un segundo.
+
+const DigitalClock = () => {
+    const [clockState, setClockState] = useState();
+  useEffect(()=>{
+    setInterval(()=>{
+        const date = new Date();
+        setClockState(date.toLocaleTimeString());
+
+    }, 1000);
+  }, []);
+    return (
+    <div className='digital-clock'>
+        <h2>{clockState}</h2>
+        </div>
+  )
+}
+
+export default DigitalClock;
