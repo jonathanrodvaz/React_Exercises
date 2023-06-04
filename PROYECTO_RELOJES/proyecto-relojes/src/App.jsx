789d7db7ebcba7d51486979f868ffb7beb850743
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-06-04 12:54:26 +0200

    proyecto_relojes, WIP

--- PROYECTO_RELOJES/proyecto-relojes/src/App.jsx
+++ PROYECTO_RELOJES/proyecto-relojes/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import DigitalClock from './components/DigitalClock/DigitalClock.jsx'
+import Countdown from './components/Countdown/Countdown.jsx'
+import Stopwatch from './components/Stopwatch/Stopwatch.jsx'
 import './App.css'
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
   return (
     <>
       <DigitalClock digitalClock={DigitalClock}/>
+      <Countdown countdown={Countdown} />
+      <Stopwatch stopwatch={Stopwatch} />
     </>
   )
 }
--- PROYECTO_RELOJES/proyecto-relojes/src/components/DigitalClock/DigitalClock.jsx
+++ PROYECTO_RELOJES/proyecto-relojes/src/components/DigitalClock/DigitalClock.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'; //Debemos traernos useState para que seteé la hora a traves de una función, y por otro lado debemos traernos useEffect para que recoja la hora local a traves de una función y la ejecute al cargar la pagina y al pasar a traves del intervalo de un segundo.
-
+import './DigitalClock.css';
 const DigitalClock = () => {
     const [clockState, setClockState] = useState();
   useEffect(()=>{
